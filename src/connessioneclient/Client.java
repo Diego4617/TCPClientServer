90237403af8b098bd89255c2dd9825dfe6784619
Diego4617 <diegogramaccia7@gmail.com>
2020-01-20 09:16:57 +0100

    20/01/2020 - TCPClientServer

---
+++ src/connessioneclient/Client.java
@@ -0,0 +1,28 @@
+package connessioneclient;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Client {
+
+    public static void main(String[] args) 
+    {
+        Socket socket = null;
+        try {
+            socket = new Socket("localhost",2000);
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    
+}
